f6e651e575e5b53613a515f808e070cdb2f51ca4
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-05-22 21:05:54 +0300

    Задал начальную структура проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todo} from './Todo';
 
-function App() {
+const App = () => {
+
+  const task1 = [
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "ReactJS", isDone: false},
+  ]
+
+  const task2 = [
+    {id: 1, title: "Hello", isDone: false},
+    {id: 2, title: "Todo", isDone: true},
+    {id: 3, title: "Task", isDone: false},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo title="What to learn" tasks={task1} />
+      <Todo title="What to buy" tasks={task2} />
     </div>
   );
 }
